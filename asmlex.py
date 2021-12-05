f056a141cca5267ae22ddcf6a4314002b5a2ec59
nabefuta <satoshinabe@220@gmail.com>
2021-12-05 22:08:44 +0900

    ソフト部分の実装 Fixes #2

--- asmlex.py
+++ asmlex.py
@@ -1,12 +1,45 @@
 import ply.lex as lex
 
 
+<<<<<<< Updated upstream
 tokens=('MOV', # move
 )
+=======
+tokens = ('MOV',  # レジスタ間のデータ転送
+
+          'LDI',  # 即値からレジスタへのデータ転送
+          'HALT',  # 停止
+          'FUCI',  # オペラントとレジスタとの演算
+          'LOAD',  # メモリからレジスタへのデータ転送
+          'STA',  # レジスタからメモリへのデータ転送
+          'FUNC',  # レジスタとメモリとの演算
+          'OUT',  # 出力
+          'RESISTER',  # レジスタ
+          'VALUE',  # 値
+>>>>>>> Stashed changes
 
 
 
+<<<<<<< Updated upstream
 t_MOV=r'MOV'
+=======
+
+t_MOV = r'MOV'
+t_FUNC = r'FUNC'
+t_LDI = r'LDI'
+t_FUCI = r'FUCI'
+t_LOAD = r'LOAD'
+t_STA = r'STA'
+t_HALT = r'HALT'
+t_OUT = r'OUT'
+t_RESISTER = r'R[0-7]'
+
+
+def t_VALUE(t):
+    r'([0-9A-Fa-f]+H)|\d+'
+    t.value = int(t.value[:-1], 16) if t.value[-1] == 'H' else int(t.value, 10)
+    return t
+>>>>>>> Stashed changes
 
 
 def t_newline(t):
--- asmyacc.py
+++ asmyacc.py
@@ -24,30 +24,132 @@ ZERO = 0
 def calc(value1: int, value2: int, operator: int):
     global RESISTER
     global OVERFLOW, CARRY, SIGN, ZERO
+<<<<<<< Updated upstream
     operator %= 8
+=======
+    value1_sign = (value1 & ((1 << MEMORY_BIT-1)-1)) + (-1) * \
+        (bool(value1 & (1 << MEMORY_BIT-1)) << MEMORY_BIT-1)
+    value2_sign = (value2 & ((1 << MEMORY_BIT-1)-1))+(-1) * \
+        (bool(value2 & (1 << MEMORY_BIT-1)) << MEMORY_BIT-1)
+    logger.info("value1:\t%d\t(%d)\t%s", value1, value1_sign, bin(value1))
+    logger.info("value2:\t%d\t(%d)\t%s", value2, value2_sign, bin(value2))
+    operator = operator % 8
+>>>>>>> Stashed changes
     result = 0
+    result_sign = 0
     if operator == 0:
         result = value1+1
+        result_sign = value1_sign+1
     if operator == 1:
         result = value1+value2
+        result_sign = value1_sign+value2_sign
     if operator == 2:
         result = value1-value2
+        result_sign = value1_sign-value2_sign
     if operator == 3:
         result = value1-1
+        result_sign = value1_sign-1
     if operator == 4:
         result = value1 | value2
+        result_sign = value1 | value2
     if operator == 5:
         result = value1 ^ value2
+        result_sign = value1 ^ value2
     if operator == 6:
         result = value1 & value2
+        result_sign = value1 & value2
     if operator == 7:
         result = ~value1
+<<<<<<< Updated upstream
     SIGN = result < 0
     ZERO = result == 0
     CARRY=not (0 <=result <(1<<MEMORY_BIT))
     OVERFLOW = not(-(1 << MEMORY_BIT) <= result <= ((1 << MEMORY_BIT)-1))
     result &= ((1 << MEMORY_BIT)-1)
     RESISTER['1'] = result
+=======
+        result_sign = ~value1
+
+    logger.info('result:\t%d\t(%d)\t%s',
+                result, result_sign, bin(result))
+    result_fixed = result & ((1 << MEMORY_BIT)-1)
+    result_sign_fixed = (result_fixed & ((1 << MEMORY_BIT-1)-1))+(-1) * \
+        (bool(result_fixed & (1 << MEMORY_BIT-1)) << MEMORY_BIT-1)
+    logger.info('fixed:\t%d\t(%d)\t%s',
+                result_fixed, result_sign_fixed, bin(result_fixed))
+    CARRY = result != result_fixed
+    OVERFLOW = result_sign != result_sign_fixed
+
+    SIGN = result_sign_fixed < 0
+    ZERO = result == 0
+    resister['R1'] = result
+    logger.info("sign : %d , zero : %d , carry : %d , overflow : %d",
+                SIGN, ZERO, CARRY, OVERFLOW)
+
+
+def p_mov(p):
+    'cmd : MOV RESISTER RESISTER'
+    global resister
+    resister[p[2]] = resister[p[3]]
+
+
+def p_func_r(p):
+    'cmd : FUNC VALUE RESISTER'
+    global resister
+    logger.info('R1 :\t%d,\t%s :\t %d,\top :\t%d',
+                resister['R1'], p[3], resister[p[3]], p[2])
+    calc(resister['R1'], resister[p[3]], p[2])
+
+
+def p_ldi(p):
+    'cmd : LDI RESISTER VALUE'
+    global resister
+    resister[p[2]] = p[3]
+    logger.info('%s :\t%d\t(%d)\t%s', p[2],
+                (p[3] & ((1 << MEMORY_BIT-1)-1))+(-1)*(bool(p[3] & (1 << MEMORY_BIT-1)) << MEMORY_BIT-1), p[3], bin(p[3]))
+
+
+def p_fuci(p):
+    'cmd : FUCI VALUE VALUE'
+    global resister
+    calc(resister['R1'], p[3], p[2])
+
+
+def p_load(p):
+    'cmd : LOAD RESISTER VALUE'
+    global resister, memory
+    resister[p[2]] = memory[p[3]]
+
+
+def p_sta(p):
+    'cmd : STA RESISTER VALUE'
+    global resister, memory
+    memory[p[3]] = resister[p[2]]
+
+
+def p_func(p):
+    'cmd : FUNC VALUE VALUE'
+    global resister, memory
+    calc(resister['R1'], memory[p[3]], p[2])
+
+
+def p_halt(p):
+    'cmd : HALT'
+    sys.exit()
+
+
+def p_out(p):
+    'cmd : OUT RESISTER VALUE'
+    global resister
+    if p[3] == 0:
+        print(resister[p[2]])
+    else:
+        raise NotImplementedError('device %s is not resistered!', p[2])
+
+
+def p_error(p):
+    print("Syntax error in input!")
+>>>>>>> Stashed changes
 
 
 if len(args) == 2:
