3d09d6499ca27759c06a93ee0ef55ed50e9ad5c8
nabefuta <satoshinabe@220@gmail.com>
2021-12-06 19:57:25 +0900

    エラー時の行数を追加

--- asmlex.py
+++ asmlex.py
@@ -40,8 +40,9 @@ def t_newline(t):
 t_ignore = ' \t'
 
 
+
 def t_error(t):
-    print("Illegal character '%s'" % t.value[0])
+    print(f"at line {t.lexer.lineno}:Illegal character '{t.value[0]}'")
     t.lexer.skip(1)
 
 
