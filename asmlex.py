45351664e1d26b7fef8b78f97923526bd95cd4e8
nabefuta <satoshinabe@220@gmail.com>
2021-12-07 23:51:05 +0900

    fixed label_out

--- asmlex.py
+++ asmlex.py
@@ -1,14 +1,17 @@
+from logging import FATAL
 import ply.lex as lex
 
-
+import re
 t_MOV = 'MOV'
 t_FUNC = 'FUNC'
 t_LDI = 'LDI'
 t_FUCI = 'FUCI'
 t_LOAD = 'LOAD'
 t_STA = 'STA'
+t_JMP = 'JMP'
 t_HALT = 'HALT'
 t_OUT = 'OUT'
+
 t_RESISTER = r'R[0-7]'
 
 reserved = {
@@ -18,6 +21,7 @@ reserved = {
     'LOAD': t_LOAD,  # メモリからレジスタへのデータ転送
     'STA': t_STA,  # レジスタからメモリへのデータ転送
     'FUNC': t_FUNC,  # レジスタとメモリとの演算
+    'JMP': t_JMP,  # 特定の番地に飛ばす
     'OUT': t_OUT,  # 出力
     'HALT': t_HALT,  # 停止
 }
@@ -30,6 +34,19 @@ tokens = [
 ] + list(reserved.values())
 
 
+def t_LABEL_OUT(t):
+    r'[0-9A-Za-z]+$'
+    t.type = reserved.get(t.value, 'LABEL_OUT')
+    if re.fullmatch(r'R[0-7]', t.value):
+        t.type = 'RESISTER'
+    if re.fullmatch(r'([0-9A-Fa-f]+H)|\d+', t.value):
+        t.type = 'VALUE'
+        return t_VALUE(t)
+    if t.type != 'LABEL_OUT' and False:
+        print(f"at line {t.lexer.lineno} : '{t.value}' can't use as label")
+        t.lexer.skip(1)
+    return t
+
 def t_VALUE(t):
     r'([0-9A-Fa-f]+H)|\d+'
     t.value = int(t.value[:-1], 16) if t.value[-1] == 'H' else int(t.value, 10)
@@ -39,12 +56,15 @@ def t_VALUE(t):
 def t_LABEL_IN(t):
     r'[0-9A-Za-z]+:'
     t.value = t.value[:-1]
-    t.type = reserved.get(t.value,'LABEL_IN')
-    
+    t.type = reserved.get(t.value, 'LABEL_IN')
+
     if t.type != 'LABEL_IN':
         print(f"at line {t.lexer.lineno} : '{t.value}' can't use as label")
         t.lexer.skip(1)
     return t
+
+
+
 
 
 def t_newline(t):
@@ -53,10 +73,12 @@ def t_newline(t):
 
 
 t_ignore = ' \t'
-t_ignore_COMMENT = r'\#.*'
+
+t_ignore_comment = r'\#.*'
+
 
 def t_error(t):
-    print(f"at line {t.lexer.lineno}:Illegal character '{t.value[0]}'")
+    print(f"at line {t.lexer.lineno}:Illegal character '{t.value}'")
     t.lexer.skip(1)
 
 
@@ -64,10 +86,10 @@ lexer = lex.lex()
 
 
 def bebug(lexer, data):
-    datas = data.split('\n') #改行ごとに区切る
+    datas = data.split('\n')  # 改行ごとに区切る
     print(f"command : {datas}")
     for command in datas:
-        if not command : #空文字列を飛ばす
+        if not command:  # 空文字列を飛ばす
             continue
         lexer.input(command)  # dataを読み込む
         while True:
